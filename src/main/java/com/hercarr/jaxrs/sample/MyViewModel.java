6a99c636559d19f563d7bee8d0ceac1b6c8a4492
Víctor Hernández <victor.hernandez@qacg.com>
2016-02-27 12:48:07 -0600

    se muestra listado de usuarios

--- src/main/java/com/hercarr/jaxrs/sample/MyViewModel.java
+++ src/main/java/com/hercarr/jaxrs/sample/MyViewModel.java
@@ -1,13 +1,18 @@
 package com.hercarr.jaxrs.sample;
 
+import java.util.List;
+
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 
+import com.hercarr.jaxrs.sample.model.User;
+import com.hercarr.jaxrs.sample.model.UserService;
+
 public class MyViewModel {
 
 	private int count;
-
+	
 	@Init
 	public void init() {
 		count = 100;
@@ -22,4 +27,9 @@ public class MyViewModel {
 	public int getCount() {
 		return count;
 	}
+	
+	public List<User> getUsers() {
+		return UserService.users;
+	}
+	
 }
